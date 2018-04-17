43a54d8b22ebc157a6644129ffa6911c4a2568ab
stephen butterfill <stephen.butterfill@gmail.com>
2018-04-17 13:51:30 +0100

    [no message]

--- README.md
+++
@@ -1,1 +0,0 @@
-Lecture notes and things.  Not easy to decode in their current format.  Contact me before using.
--- www.slides/s3sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-s3cmd sync --delete-removed out/ s3://origins-of-mind.butterfill.com --add-header "Cache-Control: max-age=86400"
-
-
